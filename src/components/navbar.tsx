e99e3e135210c6c51ad6371a5bf8658dc5ac6a39
lahiruyasas7 <lahiruyasas7@gmail.com>
2023-12-04 00:26:25 +0530

    navbar-commit

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,25 @@
-const navbar = () => {
-  return <div>navbar</div>;
+import { Nav, NavItem, NavLink } from "reactstrap";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Nav>
+        <NavItem>
+          <NavLink active href="/">
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/rated">Rated</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/auth">
+            Auth
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </div>
+  );
 };
 
-export default navbar;
+export default Navbar;
