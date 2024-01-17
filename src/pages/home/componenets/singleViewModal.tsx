88e2bd88f9badb0ff161a7c1f95204081a5b4499
lahiruyasas7 <lahiruyasas7@gmail.com>
2024-01-17 17:03:19 +0530

    starRating-commit

--- src/pages/home/componenets/singleViewModal.tsx
+++ src/pages/home/componenets/singleViewModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Rating } from "react-simple-star-rating";
 import { Col, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
 
 interface SingleViewModalProps {
@@ -7,22 +8,40 @@ interface SingleViewModalProps {
   selectedMovieData: any;
 }
 
-const SingleViewModal: React.FC<SingleViewModalProps> = ({ modal, toggle, selectedMovieData }) => {
+const SingleViewModal: React.FC<SingleViewModalProps> = ({
+  modal,
+  toggle,
+  selectedMovieData,
+}) => {
+  // const convertedRating = selectedMovieData?.vote_average / 2;
+  // console.log("converted", convertedRating);
   return (
     <Modal isOpen={modal} toggle={toggle} centered size="lg">
       <ModalHeader toggle={toggle}>{selectedMovieData?.title}</ModalHeader>
-      <ModalBody >
+      <ModalBody>
         <Row>
           <Col lg={6}>
-          <img
-          className="w-full object-fill rounded"
-          style={{ height: "auto" }}
-          alt="Sample"
-          src={`https://image.tmdb.org/t/p/w500${selectedMovieData?.poster_path}`}
-        />
+            <img
+              className="w-full object-fill rounded"
+              style={{ height: "auto" }}
+              alt="Sample"
+              src={`https://image.tmdb.org/t/p/w500${selectedMovieData?.poster_path}`}
+            />
           </Col>
           <Col lg={6}>
             <h5>{selectedMovieData?.overview}</h5>
+            <Rating
+              initialValue={selectedMovieData?.vote_average}
+              readonly
+              iconsCount={10}
+              size={25}
+              allowFraction
+              SVGstyle={{ display: "inline" }}
+            />
+            <div className="d-flex gap-2 mt-2 items-center">
+              <h4>{selectedMovieData?.vote_average}</h4>
+              <h5>({selectedMovieData?.vote_count})</h5>
+            </div>
           </Col>
         </Row>
       </ModalBody>
