f42c06589ce918280014189560e7f9833ecf3803
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-14 13:04:02 +0530

    created axios interceptor

---
+++ src/utils/axios.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+export const API = axios.create({
+	baseURL: `${process.env.REACT_APP_API_URL}/api/v1/`,
+	headers: {
+		'Content-Type': 'application/json',
+		Accept: 'application/json',
+		'Accept-Language': 'en',
+		'Content-Language': 'en',
+	},
+});
+
+API.interceptors.request.use(
+	async function (config) {
+		const bearerToken = localStorage.getItem('access_token');
+		config.headers['Authorization'] = `Bearer ${bearerToken}`;
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error);
+	},
+);
+
+API.interceptors.response.use(
+	function (response) {
+		return response;
+	},
+	async function (error) {
+		if (error.response) {
+			const errorCode = error.response.status;
+			if (errorCode === 401) {
+				console.log(error.response);
+			}
+		}
+		return Promise.reject(error);
+	},
+);
