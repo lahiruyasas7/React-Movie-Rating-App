229dcae184f29ca797765be3398505851424c713
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-14 15:39:09 +0530

    developed login handler

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,4 +1,3 @@
-
 import {
   Card,
   Row,
@@ -12,12 +11,21 @@ import {
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Controller, useForm } from "react-hook-form";
+//import { dispatch } from "../../utils/common-hooks";
+import { loginListen } from "../../redux/actions";
+import { useDispatch } from "react-redux";
 
 const Auth = () => {
+  interface LoginDataType {
+    password: string;
+    loginEmail: string;
+  }
   const defaultValues = {
     password: "",
     loginEmail: "",
   };
+
+  const dispatch = useDispatch();
 
   const schema = yup
     .object()
@@ -38,61 +46,68 @@ const Auth = () => {
     formState: { errors },
   } = useForm({ defaultValues, resolver: yupResolver(schema), mode: "all" });
 
-  const loginHandle = () => {
-    console.log("hello");
+  const loginHandle = (data: LoginDataType) => {
+    const payload = {
+      email: data.loginEmail,
+      password: data.password,
+    };
+    if (data) {
+      dispatch(loginListen(payload, history));
+    }
   };
 
   return (
     <div className="flex justify-center my-3 items-center">
       <Card className="lg:w-1/2 p-4">
         <Row className="mb-2 text-center " lg={12}>
-          <h4 className="text-xs md:text-xl">Welcome! You Can Login By registering</h4>
+          <h4 className="text-xs md:text-xl">
+            Welcome! You Can Login By registering
+          </h4>
         </Row>
         <Form onSubmit={handleSubmit(loginHandle)}>
           <Row lg={12}>
-          <Col className="m-2">
-            <Label className="font-semibold">User Name</Label>
-            <Controller
-              name="loginEmail"
-              control={control}
-              render={({ field }) => (
-                <Col>
-                  <Input
-                    id="loginEmail"
-                    type="email"
-                    placeholder="john@example.com"
-                    invalid={!!errors.loginEmail}
-                    {...field}
-                  />
-                  <FormFeedback>
-                    The email address you provided is not valid. Please make
-                    sure you use a valid email format (e.g., name@example.com)
-                  </FormFeedback>
-                </Col>
-              )}
-            />
-           
-          </Col>
+            <Col className="m-2">
+              <Label className="font-semibold">User Name</Label>
+              <Controller
+                name="loginEmail"
+                control={control}
+                render={({ field }) => (
+                  <Col>
+                    <Input
+                      id="loginEmail"
+                      type="email"
+                      placeholder="john@example.com"
+                      invalid={!!errors.loginEmail}
+                      {...field}
+                    />
+                    <FormFeedback>
+                      The email address you provided is not valid. Please make
+                      sure you use a valid email format (e.g., name@example.com)
+                    </FormFeedback>
+                  </Col>
+                )}
+              />
+            </Col>
           </Row>
           <Row className="mt-" lg={12}>
-                <Col className="m-2">
-                <Label className="font-semibold">Password</Label>
-            <Controller
-              name="password"
-              control={control}
-              render={({ field }) => (
-                <Col>
-                  <Input
-                    id="password"
-                    className="input-group-merge"
-                    invalid={!!errors.password}
-                    {...field}
-                  />
-                  <FormFeedback>Please enter password</FormFeedback>
-                </Col>
-              )}
-            />
-                </Col>
+            <Col className="m-2">
+              <Label className="font-semibold">Password</Label>
+              <Controller
+                name="password"
+                control={control}
+                render={({ field }) => (
+                  <Col>
+                    <Input
+                      id="password"
+                      className="input-group-merge"
+                      invalid={!!errors.password}
+                      {...field}
+                    />
+                    <FormFeedback>Please enter password</FormFeedback>
+                  </Col>
+                )}
+              />
+            </Col>
           </Row>
           <Row>
             <Col>
