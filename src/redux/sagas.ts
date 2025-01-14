21f0358d687b571df09c12607863a240446970bf
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-14 13:32:39 +0530

    created saga for login user

--- src/redux/sagas.ts
+++ src/redux/sagas.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { put, takeLatest } from "redux-saga/effects";
 import { actionTypes } from "./actions";
+import { API } from "../utils/axios";
+import { fireAlertError } from "../utils/customUtil";
+import { USER_ITEM } from "../utils/constants";
 
 export function* getAllMovies() {
   try {
@@ -15,6 +18,37 @@ export function* getAllMovies() {
   }
 }
 
+export function* loginUser(action: any) {
+	const { data } = action;
+	try {
+		yield put({
+			type: actionTypes.LOGIN_LOADER_HANDLE,
+			payload: true,
+		});
+
+		yield API.post(`auth/login`, data).then((res) => {
+			window.localStorage.setItem(USER_ITEM, JSON.stringify(res?.data));
+			location.replace('/');
+		});
+		yield put({
+			type: actionTypes.LOGIN_LOADER_HANDLE,
+			payload: false,
+		});
+	} catch (e: any) {
+		yield put({
+			type: actionTypes.LOGIN_LOADER_HANDLE,
+			payload: false,
+		});
+		yield put({
+			type: actionTypes.IS_LOGIN_ERROR,
+			data: true,
+		});
+		fireAlertError('Error', e.response?.data?.message || 'Error in login');
+		console.error(e);
+	}
+}
+
 export default function* rootSaga() {
     yield takeLatest(actionTypes.GET_ALL_MOVIES, getAllMovies);
+    yield takeLatest(actionTypes.LOGIN_LISTEN, loginUser);
 }
