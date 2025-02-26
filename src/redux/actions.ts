7e54a9c7c912089ce19c97a5281a0cd6ef6073e5
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-02-26 23:11:36 +0530

    created redux flow for register user

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,35 +1,53 @@
 export const actionTypes = {
-    GET_ALL_MOVIES: 'GET_ALL_MOVIES',
-    GET_MOVIES_SUCCESS: 'GET_MOVIES_SUCCESS',
-    GET_MOVIES_FAIL: 'GET_MOVIES_FAIL',
-    LOGIN_LISTEN: 'LOGIN_LISTEN',
-    LOGIN_SUCCESS: 'LOGIN_SUCCESS',
-    LOGIN_LOADER_HANDLE: 'LOGIN_LOADER_HANDLE',
-    IS_LOGIN_ERROR: 'IS_LOGIN_ERROR',
+  GET_ALL_MOVIES: "GET_ALL_MOVIES",
+  GET_MOVIES_SUCCESS: "GET_MOVIES_SUCCESS",
+  GET_MOVIES_FAIL: "GET_MOVIES_FAIL",
+  LOGIN_LISTEN: "LOGIN_LISTEN",
+  LOGIN_SUCCESS: "LOGIN_SUCCESS",
+  LOGIN_LOADER_HANDLE: "LOGIN_LOADER_HANDLE",
+  IS_LOGIN_ERROR: "IS_LOGIN_ERROR",
+  REGISTER_USER: "REGISTER_USER",
+};
+
+export interface registerDataType {
+  email: string;
+  firstName: string;
+  lastName: string;
+  phone?: string;
+  address?: string;
+  dateOfBirth?: string;
+  password?: string;
 }
 
-export function getAllMovies () {
-    return { type: actionTypes.GET_ALL_MOVIES};
+export function getAllMovies() {
+  return { type: actionTypes.GET_ALL_MOVIES };
 }
 
 export const loginListen = (data: any, history: any) => {
-	return {
-		type: actionTypes.LOGIN_LISTEN,
-		data,
-		history,
-	};
+  return {
+    type: actionTypes.LOGIN_LISTEN,
+    data,
+    history,
+  };
 };
 
 export const loginSuccess = (data: any) => {
-	return {
-		type: actionTypes.LOGIN_SUCCESS,
-		data,
-	};
+  return {
+    type: actionTypes.LOGIN_SUCCESS,
+    data,
+  };
 };
 
 export const handleLoginLoader = (data: any) => {
-	return {
-		type: actionTypes.LOGIN_LOADER_HANDLE,
-		data,
-	};
+  return {
+    type: actionTypes.LOGIN_LOADER_HANDLE,
+    data,
+  };
+};
+
+export const registerUser = (payload: registerDataType) => {
+  return {
+    type: actionTypes.REGISTER_USER,
+    payload,
+  };
 };
