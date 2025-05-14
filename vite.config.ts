cfae269434b66d2058b65691a9dee5e42c2b54c0
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-05-14 23:16:15 +0530

    added testing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Allows using `describe`, `it`, `expect` without import
+    environment: 'jsdom', // Simulates browser environment
+    setupFiles: './src/setupTests.ts', // Your test setup
+  },
 })
