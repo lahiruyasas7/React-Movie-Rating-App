2b5b407f08f2d0c54b518b49fcf317ca3fc5631f
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-20 13:58:02 +0530

    removed unused code parts

--- src/pages/home/componenets/movieCard.tsx
+++ src/pages/home/componenets/movieCard.tsx
@@ -5,29 +5,6 @@ import { Star } from "react-feather";
 const MovieCard = ({ movieData }: any) => {
   console.log("movieData", movieData);
   return (
-    // <div style={{cursor: 'pointer'}}>
-    //   <Card className="movie-card shadow overflow-hidden cursor-pointer clickable" style={{ width: "300px", height: "400px" }}>
-    //     <img
-    //       className="w-full object-fill rounded custom-placeholder"
-    //       style={{ height: "250px" }}
-    //       alt="Sample"
-    //       src={`https://image.tmdb.org/t/p/w500${movieData?.poster_path}`}
-    //     />
-    //     <CardBody>
-    //       <CardTitle tag="h5">{movieData?.title}</CardTitle>
-
-    //       <Label>{`Release data ${movieData?.release_date}`}</Label>
-    //       <Row>
-    //         <Col>
-    //           <h6>{`Ratings ${Number(movieData?.vote_average).toFixed(1)}`}</h6>
-    //         </Col>
-    //         <Col>
-    //           <h6>{`Vote Count ${movieData?.vote_count}`}</h6>
-    //         </Col>
-    //       </Row>
-    //     </CardBody>
-    //   </Card>
-    // </div>
     <div className="transition-transform transform hover:scale-105 duration-300 ease-in-out">
       <Card
         className="movie-card shadow-lg rounded-2xl overflow-hidden hover:shadow-2xl bg-white/10 backdrop-blur-lg border border-gray-200"
--- src/pages/home/componenets/singleViewModal.tsx
+++ src/pages/home/componenets/singleViewModal.tsx
@@ -18,36 +18,7 @@ const SingleViewModal: React.FC<SingleViewModalProps> = ({
   // console.log("converted", convertedRating);
   console.log("selectedMovieData", selectedMovieData);
   return (
-    // <Modal isOpen={modal} toggle={toggle} centered size="lg">
-    //   <ModalHeader toggle={toggle}>{selectedMovieData?.title}</ModalHeader>
-    //   <ModalBody>
-    //     <Row>
-    //       <Col lg={6}>
-    //         <img
-    //           className="w-full object-fill rounded"
-    //           style={{ height: "auto" }}
-    //           alt="Sample"
-    //           src={`https://image.tmdb.org/t/p/w500${selectedMovieData?.poster_path}`}
-    //         />
-    //       </Col>
-    //       <Col lg={6}>
-    //         <h5>{selectedMovieData?.overview}</h5>
-    //         <Rating
-    //           initialValue={selectedMovieData?.vote_average}
-    //           readonly
-    //           iconsCount={10}
-    //           size={25}
-    //           allowFraction
-    //           SVGstyle={{ display: "inline" }}
-    //         />
-    //         <div className="d-flex gap-2 mt-2 items-center">
-    //           <h4>{Number(selectedMovieData?.vote_average).toFixed(1)}</h4>
-    //           <h5>({selectedMovieData?.vote_count})</h5>
-    //         </div>
-    //       </Col>
-    //     </Row>
-    //   </ModalBody>
-    // </Modal>
+  
     <Modal isOpen={modal} toggle={toggle} centered size="lg">
       <ModalHeader toggle={toggle} className="font-semibold text-lg">
         {selectedMovieData?.title}
