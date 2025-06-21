47ff838da7d10884375b9dd157d2dbea7b3ced61
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-21 12:51:05 +0530

    feat: changed bg color of modal and text colors

--- src/pages/home/componenets/singleViewModal.tsx
+++ src/pages/home/componenets/singleViewModal.tsx
@@ -14,71 +14,69 @@ const SingleViewModal: React.FC<SingleViewModalProps> = ({
   toggle,
   selectedMovieData,
 }) => {
-  // const convertedRating = selectedMovieData?.vote_average / 2;
-  // console.log("converted", convertedRating);
-  console.log("selectedMovieData", selectedMovieData);
   return (
-  
     <Modal isOpen={modal} toggle={toggle} centered size="lg">
-      <ModalHeader toggle={toggle} className="font-semibold text-lg">
-        {selectedMovieData?.title}
-      </ModalHeader>
-      <ModalBody>
-        <Row>
-          <Col lg={6}>
-            <img
-              className="w-full object-cover rounded-lg"
-              style={{ height: "auto" }}
-              alt={selectedMovieData?.title}
-              src={`https://image.tmdb.org/t/p/w500${selectedMovieData?.poster_path}`}
-            />
-          </Col>
-          <Col lg={6} className="space-y-3">
-            <h5 className="text-gray-800 font-medium leading-relaxed">
-              {selectedMovieData?.overview}
-            </h5>
-
-            {/* Rating */}
-            <div className="mt-2">
-              <Rating
-                initialValue={selectedMovieData?.vote_average}
-                readonly
-                iconsCount={10}
-                size={25}
-                allowFraction
-                SVGstyle={{ display: "inline" }}
+      <div className="bg-gradient-to-br from-gray-800 via-zinc-900 to-black text-white rounded-lg">
+        <ModalHeader toggle={toggle} className="font-semibold text-lg">
+          {selectedMovieData?.title}
+        </ModalHeader>
+        <ModalBody>
+          <Row>
+            <Col lg={6}>
+              <img
+                className="w-full object-cover rounded-lg"
+                style={{ height: "auto" }}
+                alt={selectedMovieData?.title}
+                src={`https://image.tmdb.org/t/p/w500${selectedMovieData?.poster_path}`}
               />
-              <div className="flex items-center gap-3 mt-1">
-                <Star className="text-yellow-500 w-5 h-5" />
-                <span className="text-lg font-bold">
-                  {Number(selectedMovieData?.vote_average).toFixed(1)}
-                </span>
-                <span className="text-sm text-gray-500">
-                  ({selectedMovieData?.vote_count} votes)
-                </span>
-              </div>
-            </div>
+            </Col>
+            <Col lg={6} className="space-y-3">
+              <h5 className="text-gray-300 font-medium leading-relaxed">
+                {selectedMovieData?.overview}
+              </h5>
 
-            {/* Additional Info */}
-            <div className="space-y-2 mt-4 text-sm text-gray-700">
-              <div className="flex items-center gap-2">
-                <Calendar className="w-4 h-4 text-gray-500" />
-                <span>
-                  <strong>Release Date:</strong>{" "}
-                  {selectedMovieData?.release_date}
-                </span>
+              {/* Rating */}
+              <div className="mt-2">
+                <Rating
+                  initialValue={selectedMovieData?.vote_average}
+                  readonly
+                  iconsCount={10}
+                  size={25}
+                  allowFraction
+                  SVGstyle={{ display: "inline" }}
+                />
+                <div className="flex items-center gap-3 mt-1">
+                  <Star className="text-yellow-500 w-5 h-5" />
+                  <span className="text-lg font-bold">
+                    {Number(selectedMovieData?.vote_average).toFixed(1)}
+                  </span>
+                  <span className="text-sm text-gray-400">
+                    ({selectedMovieData?.vote_count} votes)
+                  </span>
+                </div>
               </div>
-              <div className="flex items-center gap-2">
-                <Globe className="w-4 h-4 text-gray-500" />
-                <span>
-                  <strong>Language:</strong>{" "}
-                  {selectedMovieData?.original_language?.toUpperCase()}
-                </span>
+
+              {/* Additional Info */}
+              <div className="space-y-2 mt-4 text-sm text-gray-700">
+                <div className="flex items-center gap-2">
+                  <Calendar className="w-4 h-4 text-gray-100" />
+                  <span>
+                    <strong>Release Date:</strong>{" "}
+                    {selectedMovieData?.release_date}
+                  </span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Globe className="w-4 h-4 text-gray-100" />
+                  <span>
+                    <strong>Language:</strong>{" "}
+                    {selectedMovieData?.original_language?.toUpperCase()}
+                  </span>
+                </div>
               </div>
-            </div>
-          </Col>
-        </Row>
-      </ModalBody>
+            </Col>
+          </Row>
+        </ModalBody>
+      </div>
     </Modal>
   );
 };
