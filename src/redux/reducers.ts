3fc8046a204f978b50cb281ddbb02032d01a202b
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-23 00:19:37 +0530

    feat: created reducer for trending movies

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,33 +1,42 @@
 import { combineReducers } from "redux";
 import { actionTypes } from "./actions";
 
-
 const initialState = {
-    moviesList: null
-  };
-  
-  const reducer = (state = initialState, action: any) => {
-    switch (action.type) { 
+  moviesList: null,
+  tvSeriesList: null,
+};
 
-      case actionTypes.GET_MOVIES_SUCCESS:
-			return {
-				...state,
-				moviesList: action.data,
-			};
-      case actionTypes.GET_MOVIES_FAIL:
-			return {
-				...state,
-				moviesList: null,
-			};
-      default:
-        return state;
-    }
-  };
+const reducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case actionTypes.GET_MOVIES_SUCCESS:
+      return {
+        ...state,
+        moviesList: action.data,
+      };
+    case actionTypes.GET_MOVIES_FAIL:
+      return {
+        ...state,
+        moviesList: null,
+      };
+    case actionTypes.GET_TV_SERIES_SUCCESS:
+      return {
+        ...state,
+        moviesList: action.data,
+      };
+    case actionTypes.GET_TV_SERIES_FAIL:
+      return {
+        ...state,
+        moviesList: null,
+      };
+    default:
+      return state;
+  }
+};
 
-  const rootReducer = combineReducers({
-      reducer,
-  });
+const rootReducer = combineReducers({
+  reducer,
+});
 
-  export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof rootReducer>;
 
-  export default rootReducer;
+export default rootReducer;
