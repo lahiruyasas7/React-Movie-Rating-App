2d1b8f1aca572fbce8283db1f13d237d1bdde3c6
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-07-04 13:32:48 +0530

    feat: created google success page

---
+++ src/pages/auth/GoogleSuccess.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { USER_ITEM } from "../../utils/constants";
+
+const GoogleSuccess = () => {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = params.get("accessToken");
+    console.log("params", params);
+    if (accessToken) {
+      // Store accessToken in localStorage
+      localStorage.setItem(
+        USER_ITEM,
+        JSON.stringify({ accessToken }) // you can store more if needed
+      );
+
+      // Redirect to homepage or dashboard
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, []);
+
+  return <p>Logging you in via Google...</p>;
+};
+
+export default GoogleSuccess;
