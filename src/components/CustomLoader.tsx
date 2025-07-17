653cde4f7ca534ac450b1cd3d9ca3cc5b885541e
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-07-17 14:02:48 +0530

    feat: created custom loader ui

---
+++ src/components/CustomLoader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoaderOverlay = () => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/30">
+      <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default LoaderOverlay;
