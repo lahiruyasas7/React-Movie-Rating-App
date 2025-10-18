ab6adb51dd062fd839845c163635894bc93226dc
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-10-18 18:19:58 +0530

    feat: developed pagination for popular movies

--- src/pages/popular-movies/PopularMovies.tsx
+++ src/pages/popular-movies/PopularMovies.tsx
@@ -4,22 +4,32 @@ import { getPopularMovies } from "../../redux/actions";
 import { MovieData } from "../../types/types";
 import MovieCard from "../home/componenets/movieCard";
 import SingleViewModal from "../home/componenets/singleViewModal";
+import ReactPaginate from "react-paginate";
 
 function PopularMovies() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedMovieData, setSelectedMovieData] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
   const { popularMovies } = useSelector((state: any) => state.reducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPopularMovies(1));
-  }, []);
+    if (currentPage) {
+      dispatch(getPopularMovies(currentPage));
+    }
+  }, [dispatch, currentPage]);
   console.log("popularMovies", popularMovies);
 
   const selectedCardHandler = (data: any) => {
     setSelectedMovieData(data);
     setIsModalOpen(!isModalOpen);
   };
+
+  const handlePageChange = (data: { selected: number }) => {
+    const newPage = data.selected + 1; // react-paginate is zero-based
+    setCurrentPage(newPage);
+  };
+
   return (
     <div className="min-h-screen">
       <div className="w-full flex flex-wrap gap-12 p-5 justify-center">
@@ -31,6 +41,22 @@ function PopularMovies() {
             </div>
           ))}
       </div>
+      {popularMovies?.total_pages > 1 && (
+        <div className="flex justify-center my-6">
+          <ReactPaginate
+            previousLabel={"← Previous"}
+            nextLabel={"Next →"}
+            breakLabel={"..."}
+            pageCount={popularMovies.total_pages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={handlePageChange}
+            containerClassName={"pagination flex gap-2"}
+            activeClassName={"active text-blue-500 font-semibold"}
+            forcePage={currentPage - 1} // keeps pagination in sync with state
+          />
+        </div>
+      )}
       <SingleViewModal
         modal={isModalOpen}
         toggle={selectedCardHandler}
