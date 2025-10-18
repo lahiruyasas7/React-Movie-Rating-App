0a52c892592a39f1849a864c1279fefa2a3422c7
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-10-18 13:44:27 +0530

    feat: created movie data type

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,3 +14,16 @@ export interface tvSeriesCardType {
   vote_average: number;
   vote_count: number;
 }
+  export interface MovieData {
+    adult: boolean;
+    id: number;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    vote_average: number;
+    vote_count: number;
+  }
+
