aaf67c02790bd75204758109423ba85f5388e209
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-10-18 13:42:53 +0530

    feat: created ui for popular movies and integrated data

--- src/pages/popular-movies/PopularMovies.tsx
+++ src/pages/popular-movies/PopularMovies.tsx
@@ -1,16 +1,30 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPopularMovies } from "../../redux/actions";
+import { MovieData } from "../../types/types";
+import MovieCard from "../home/componenets/movieCard";
 
 function PopularMovies() {
-  const { PopularMovies } = useSelector((state: any) => state.reducer);
+  const { popularMovies } = useSelector((state: any) => state.reducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPopularMovies(1));
   }, []);
-  console.log("popularMovies", PopularMovies);
-  return <div>PopularMovies</div>;
+  console.log("popularMovies", popularMovies);
+  return (
+    <div className="min-h-screen">
+      <div className="w-full flex flex-wrap gap-12 p-5 justify-center">
+        {popularMovies &&
+          popularMovies.results.map((data: MovieData) => (
+            <div key={data.id}>
+              {" "}
+              <MovieCard key={data.id} movieData={data} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default PopularMovies;
