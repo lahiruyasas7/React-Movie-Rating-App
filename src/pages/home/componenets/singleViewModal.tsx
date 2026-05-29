dca07f24814e46ffc06c2dfef57fb2792c60371f
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-05-30 01:48:24 +0530

    feat: add movie type

--- src/pages/home/componenets/singleViewModal.tsx
+++ src/pages/home/componenets/singleViewModal.tsx
@@ -3,10 +3,28 @@ import { Calendar, Globe, Star } from "react-feather";
 import { Rating } from "react-simple-star-rating";
 import { Col, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
 
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  title: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  softcore: boolean;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 interface SingleViewModalProps {
   modal: boolean;
   toggle: any;
-  selectedMovieData: any;
+  selectedMovieData: Movie | null;
 }
 
 const SingleViewModal: React.FC<SingleViewModalProps> = ({
